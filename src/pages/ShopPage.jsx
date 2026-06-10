a5626ff4e34d0e04ee47f9c5a82ca27b30cc392e
Gandheesh-SA <sagandheesh1911@gmail.com>
2026-06-10 14:26:51 +0530

    Product fixed

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Footer from "../components/Footer";
 import ProductDetails from "../sub-section/ProductDetail"
-import ProductAccordion from "../sub-section/ProductMISC"
+import ProductSpecs from "../sub-section/ProductSpecs"
 import bannerImg from "../images/shop/Banner.png"
 import HowToUse from "../sub-section/HowToUse"
 import Testimonials from "../sub-section/Testimonials";
@@ -13,7 +13,7 @@ export default function ShopPage() {
   return (
   <>
      <ProductDetails />
-     <ProductAccordion />
+    <ProductSpecs />
      <div style={{ width: "100%", lineHeight: 0 }}>
   <img
     src={bannerImg}
